05940f3b46da2e91bf0e8da03548624daf304ca8
Babak Alizadeh <alzadeh.babak@gmail.com>
2026-05-07 22:44:04 +0330

    bug fix in android devices build

--- hesabixUI/hesabix_ui/macos/Flutter/GeneratedPluginRegistrant.swift
+++ hesabixUI/hesabix_ui/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,28 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import desktop_drop
-import file_picker
-import file_saver
-import file_selector_macos
-import flutter_secure_storage_macos
-import flutter_sound
-import mobile_scanner
-import printing
-import share_plus
-import shared_preferences_foundation
-import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DesktopDropPlugin.register(with: registry.registrar(forPlugin: "DesktopDropPlugin"))
-  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
-  FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
-  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  FlutterSoundPlugin.register(with: registry.registrar(forPlugin: "FlutterSoundPlugin"))
-  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
-  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
-  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
-  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
